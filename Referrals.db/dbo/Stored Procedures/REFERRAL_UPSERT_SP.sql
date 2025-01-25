23df161f8c7c71a4ff692a18bb8d09561f6893b9
David Sweeney <david.j.sweeney@outlook.com>
2025-01-25 18:08:04 -0500

    Updated stored procedure and sqlproj file

--- Referrals.db/dbo/Stored Procedures/REFERRAL_UPSERT_SP.sql
+++ Referrals.db/dbo/Stored Procedures/REFERRAL_UPSERT_SP.sql
@@ -1,70 +1,98 @@
 ﻿CREATE PROCEDURE [dbo].[REFERRAL_UPSERT_SP]
-	@referral_id int = 0,
-	@referral_date datetime2,
-	@search_zip nchar(5) NULL,
-	@parent_first_name varchar(50),
-	@parent_last_name varchar(50),
-	@parent_phone varchar(10),
-	@parent_ok_to_text bit,
-	@parent_email varchar(50),
-	@parent_comment varchar(2000) NULL,
-	@magellan_closed_date DATE NULL,
-	@magellan_contact_status varchar(50),
-	@magellan_closed_status varchar(50),
-	@magellan_closed_notes varchar(1000) NULL,
-	@magellan_closed_reason varchar(1000) NULL
+  @referral_id int = 0,
+  @referral_date datetime2,
+  @search_zip nchar(5) NULL,
+  @parent_first_name varchar(50),
+  @parent_last_name varchar(50),
+  @parent_phone varchar(10),
+  @parent_ok_to_text bit,
+  @parent_email varchar(50),
+  @parent_comment varchar(2000) NULL,
+  @magellan_closed_date DATE NULL,
+  @magellan_contact_status varchar(50),
+  @magellan_closed_status varchar(50),
+  @magellan_closed_notes varchar(1000) NULL,
+  @magellan_closed_reason varchar(1000) NULL
 AS
   BEGIN
-	
-	  DECLARE @parent_id UNIQUEIDENTIFIER
-	  DECLARE @child_id UNIQUEIDENTIFIER
 
-	  SET @parent_id = NEWID()
-	  SET @child_id = NEWID()
+    DECLARE @parent_id UNIQUEIDENTIFIER
+    DECLARE @child_id UNIQUEIDENTIFIER
 
-	  -- Insert new parent
-	  INSERT INTO [dbo].[PARENTS] (
-		id, 
-		first_name, 
-		last_name, 
-		phone, 
-		ok_to_text, 
-		email, 
-		comment, 
-		created_datetime)
-	  VALUES (
-		@parent_id, 
-		@parent_first_name, 
-		@parent_last_name, 
-		@parent_phone, 
-		@parent_ok_to_text, 
-		@parent_email, 
-		@parent_comment, 
-		GETDATE());
-	  
-	    -- Insert new referral
-	    INSERT INTO [dbo].[REFERRALS] (
-	      id,
-		  referral_id,
-		  search_zip,
-		  parent_id,
-		  referral_date,
-		  magellan_closed_date,
-		  magellan_contact_status,
-		  magellan_closed_status,
-		  magellan_closed_notes,
-		  magellan_closed_reason,
-		  created_datetime)
-	    VALUES (
-	      NEWID(),
-		  @referral_id,
-		  @search_zip,
-		  @parent_id,
-		  @referral_date,
-		  @magellan_closed_date,
-		  @magellan_contact_status,
-		  @magellan_closed_status,
-		  @magellan_closed_notes,
-		  @magellan_closed_reason,
-		  GETDATE());
-	END
+    SET @parent_id = NEWID()
+    SET @child_id = NEWID()
+
+    UPDATE [dbo].[REFERRALS]
+    SET
+    search_zip = @search_zip,
+    referral_date = @referral_date,
+    magellan_closed_date = @magellan_closed_date,
+    magellan_contact_status = @magellan_contact_status,
+    magellan_closed_status = @magellan_closed_status,
+    magellan_closed_notes = @magellan_closed_notes,
+    magellan_closed_reason = @magellan_closed_reason
+    WHERE referral_id = @referral_id
+    
+    IF @@ROWCOUNT = 0
+
+    BEGIN
+      -- Insert new parent
+      INSERT INTO [dbo].[PARENTS] (
+       id, 
+        first_name, 
+        last_name, 
+        phone, 
+        ok_to_text, 
+        email, 
+        comment, 
+        created_datetime)
+      VALUES (
+        @parent_id, 
+        @parent_first_name, 
+        @parent_last_name, 
+        @parent_phone, 
+        @parent_ok_to_text, 
+        @parent_email, 
+        @parent_comment, 
+        GETDATE());
+
+      -- Insert new referral
+      INSERT INTO [dbo].[REFERRALS] (
+        id,
+        referral_id,
+        search_zip,
+        parent_id,
+        referral_date,
+        magellan_closed_date,
+        magellan_contact_status,
+        magellan_closed_status,
+        magellan_closed_notes,
+        magellan_closed_reason,
+        created_datetime)
+      VALUES (
+        NEWID(),
+        @referral_id,
+        @search_zip,
+        @parent_id,
+        @referral_date,
+        @magellan_closed_date,
+        @magellan_contact_status,
+        @magellan_closed_status,
+        @magellan_closed_notes,
+        @magellan_closed_reason,
+        GETDATE());
+    END
+  ELSE
+    BEGIN
+      -- Update existing parent
+      UPDATE [dbo].[PARENTS]
+      SET
+        first_name = @parent_first_name,
+        last_name = @parent_last_name,
+        phone = @parent_phone,
+        ok_to_text = @parent_ok_to_text,
+        email = @parent_email,
+        comment = @parent_comment
+      WHERE id = (SELECT parent_id FROM [dbo].[REFERRALS] WHERE referral_id = @referral_id)
+    END
+  END
